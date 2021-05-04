01dd71ff571d99a6e4745cbb328370178c3f89af
JcdeA <jcde@jcde.xyz>
2021-05-05 00:30:36 +0900

    chore: fix tests so they pass

--- test/pages/index.test.tsx
+++ test/pages/index.test.tsx
@@ -1,17 +1,9 @@
 import React from 'react'
-import { render, fireEvent } from '../testUtils'
-import { Home } from '../../pages/index'
-
+import Home from '../../pages/index'
+import { render, screen } from '@testing-library/react'
 describe('Home page', () => {
-  it('matches snapshot', () => {
-    const { asFragment } = render(<Home />, {})
-    expect(asFragment()).toMatchSnapshot()
-  })
-
-  it('clicking button triggers alert', () => {
-    const { getByText } = render(<Home />, {})
-    window.alert = jest.fn()
-    fireEvent.click(getByText('Test Button'))
-    expect(window.alert).toHaveBeenCalledWith('With typescript and Jest')
+  it('Renders without crashing', () => {
+    render(<Home />, {})
+    expect(screen.getByText("Hi, I'm JcdeA")).toBeInTheDocument()
   })
 })
