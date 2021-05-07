3e04b933e418f9d7ac02d8370f3802e5132517a4
JcdeA <jcde@jcde.xyz>
2021-05-07 12:15:29 +0900

    remove animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,41 +1,18 @@
 import Link from 'next/link'
 import 'd2coding/d2coding-full.css'
-
-import Typewriter from 'typewriter-effect'
 export const Home = (): JSX.Element => (
   <>
     <h1 className="text-5xl py-2">Hi, I{"'"}m JcdeA</h1>
     <div className="text-lg md:text-xl">
-      <span className="inline-block">
-        <Typewriter
-          // eslint-disable-next-line @typescript-eslint/no-empty-function
-          onInit={() => {}}
-          options={{
-            strings: [
-              'Next.js',
-              'TypeScript',
-              'Python',
-              'Docker',
-              'Discord.js',
-              'Linux',
-              'Full stack',
-            ],
-            autoStart: true,
-            delay: 80,
-            loop: true,
-            cursor: '_',
-            cursorClassName: 'typed-cursor',
-          }}
-        />
-      </span>
-      Developer building unique experiences
+      <span className="inline-block"></span>
+      Student Developer building unique experiences
     </div>
     <Link href="/about">
       {/* w-full sm:w-auto flex-none bg-gray-900 hover:bg-gray-700 text-white text-lg leading-6 font-semibold py-3 px-6  */}
       <div className="bg-red-500 items-center justify-center hover:bg-red-400 text-white inline-flex align-center w-auto py-1 px-2 md:py-2 md:px-4  text-sm md:text-lg mt-6 mb-8 group hover:pr-2 border border-red-200 border-transparent rounded-md focus:ring-2 focus:ring-offset-2 focus:ring-offset-red-500 focus:ring-gray-900 focus:outline-none transition-colors duration-200">
         <span className="text-md leading-7">Learn more</span>
         <svg
-          className="h-6 ml-2 md:h-8 md:ml-4 group-hover:ml-2 md:group-hover:ml-6"
+          className="h-6 ml-2 md:h-8 md:ml-4  group-hover:ml-2 md:group-hover:ml-6"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
