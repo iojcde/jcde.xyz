739f338719426fb2f7d045dad20b1c60b15c9767
JcdeA <jcde@jcde.xyz>
2021-05-08 12:44:29 +0900

    feat: preload fonts

--- components/seo.tsx
+++ components/seo.tsx
@@ -3,7 +3,8 @@ const SEO: React.FC = () => {
   return (
     <Head>
       <title>JcdeA</title>
-
+      <link href="/static/fonts/D2Coding.woff2" rel="preload" as="font" />
+      <link href="/static/fonts/D2CodingBold.woff2" rel="preload" as="font" />
       <meta name="description" content="JcdeA's portfolio"></meta>
       <meta
         name="viewport"
