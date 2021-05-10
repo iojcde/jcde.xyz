d8e55c0f2f32b571dff15975c81cf2f874c34e90
JcdeA <jcde@jcde.xyz>
2021-05-10 17:14:33 +0900

    feat: inline transform

--- components/layout.tsx
+++ components/layout.tsx
@@ -11,14 +11,14 @@ const Layout: React.FC = ({ children }: LayoutProps) => {
   const notHome = router.pathname !== '/'
   return (
     <>
-      <SEO />
       <div className="font-mono z-10 w-screen px-4 dark:bg-gray-900 transition-colors duration-200 max-w-3xl mx-auto">
         {notHome && (
           <div className="mt-2">
             <Link href="/">
-              <div className=" items-center justify-center inline-flex align-center w-auto  md:py-2 md:px-4  text-sm md:text-lg mt-6 mb-8 group hover:pl-2 rounded-md focus:ring-2 focus:ring-offset-2 focus:ring-offset-red-500 focus:ring-gray-900 focus:outline-none transition-colors duration-200">
+              <div className=" items-center justify-center inline-flex align-center w-auto  md:py-2 md:px-4  text-sm md:text-lg mt-6 mb-8 group md:hover:pl-2 rounded-md focus:ring-2 focus:ring-offset-2 focus:ring-offset-red-500 focus:ring-gray-900 focus:outline-none transition-colors duration-200">
                 <svg
-                  className="transform rotate-180 h-6 mr-2 group-hover:mr-4"
+                  transform="rotate(180)"
+                  className="h-6 mr-2 md:group-hover:mr-4"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
